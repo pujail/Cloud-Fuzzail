b842cf80dd6b76164311893d27e86ece0c42a7cb
Fuzzail(TI) <perlakhero@gmail.com>
2025-11-06 14:34:07 +0700

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -19,7 +19,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getFirestore();
-window.db = db;
 
 // LOGIKA onAuthStateChanged YANG DIPERBAIKI
 onAuthStateChanged(auth, (user) => {
